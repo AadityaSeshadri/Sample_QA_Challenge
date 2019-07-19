faae9acef4a7a8796a24c4f63ba20d98ab735f4b
Aaditya Seshadri <aaditya.seshadri@prudential.com.sg>
2019-07-19 09:25:17 +0800

    Basic Maven Project Setup

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+//package com.APIComp;
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ */
+public class App {
+  
+    public static void main(String[] args) {
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&Hello World!&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+        Arrays.stream(args).forEach(System.out::println);
+        
+    }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,18 @@
+//package APIComp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void testApp() {
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&Hello World Test!&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+    }
+}
